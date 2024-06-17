27ae8354d19a5eefb0e8349d1572e8517d769526
Yogesh <yogeshkumar86077@gmail.com>
2024-06-17 14:36:33 +0530

    second

--- client/myapp/src/components/Dashboard.js
+++ client/myapp/src/components/Dashboard.js
@@ -151,6 +151,7 @@ const Dashboard = () => {
     setFilteredReviews(filtered);
   };
   const exportToExcel = () => {
+    
     const worksheet = XLSX.utils.json_to_sheet(reviews);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Reviews");
